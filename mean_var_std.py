655f36de0aa27a89d9efa9288704fa31017c8b18
Emanuel Boiteux <emanuelbboiteux@gmail.com>
2025-12-02 17:07:40 -0300

    Mean-Variance-Standard Deviation Calculator

---
+++ mean_var_std.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    matrix = np.array(list).reshape(3, 3)
+
+    result = {}
+
+    result['mean'] = [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()]
+    result['variance'] = [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()]
+    result['standard deviation'] = [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()]
+    result['max'] = [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()]
+    result['min'] = [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()]
+    result['sum'] = [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()]
+
+    return result
